226804d8624d1c635a6c84d981c238b7c257ce6f
karimeli <karimcarlos32@icloud.com>
2025-09-09 09:43:18 -0600

    tags delated

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :posts do
-    resources :comments, only: [:create, :destroy]  # Solo 'create' y 'destroy' para comentarios
+    resources :comments, only: [ :create, :destroy ]  # Solo 'create' y 'destroy' para comentarios
   end
 
-  root 'home#index'  # Página principal
+  root "home#index"  # Página principal
 end
