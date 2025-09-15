529c965d165f2af4ae863725bd44338268338dfc
karimeli <karimcarlos32@icloud.com>
2025-09-15 09:09:23 -0600

    post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ApplicationRecord
   validates :name, presence: true
   validates :title, presence: true, length: { minimum: 5 }
   validates :content, presence: true
-
+  belongs_to :user
   has_many :comments, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_11_150159) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_15_144923) do
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
     t.text "body"
@@ -26,6 +26,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_11_150159) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
