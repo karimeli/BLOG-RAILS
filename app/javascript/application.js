c977a3c1f61cc6af4acbb885c6aa69ed9c2f4e84
karimeli <karimcarlos32@icloud.com>
2025-09-18 13:14:10 -0600

    EDITOR ya funcional

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,15 +1,4 @@
-import { Turbo } from "@hotwired/turbo-rails"
+import "@hotwired/turbo-rails"
 import "controllers";
-// Inicializa CKEditor cuando la página se cargue
-document.addEventListener("turbo:load", () => {
-  const editorElements = document.querySelectorAll('.ckeditor');
-  editorElements.forEach(element => {
-    ClassicEditor.create(element)
-      .then(editor => {
-        console.log('CKEditor inicializado');
-      })
-      .catch(error => {
-        console.error('Error al inicializar CKEditor:', error);
-      });
-  });
-});
+
+
