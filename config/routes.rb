bd9c3a3af4cd0f63473c0a8ef79af4132b198f58
karimeli <karimcarlos32@icloud.com>
2025-09-24 09:27:14 -0600

    db

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   mount Ckeditor::Engine => "/ckeditor"
   devise_for :users
+
   resources :posts do
     resources :comments, only: [ :create, :edit, :update, :destroy ]
-    post :upload_image, on: :collection
+  end
+
+  resources :comments do
+    collection do
+      post :upload_image
+    end
   end
 
   root "posts#index"
