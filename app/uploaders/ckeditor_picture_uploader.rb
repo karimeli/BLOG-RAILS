f13c2da3e2a12571676f4cd92803c0ea2ddc8081
karimeli <karimcarlos32@icloud.com>
2025-09-24 12:00:28 -0600

    posible correcion

--- app/uploaders/ckeditor_picture_uploader.rb
+++ app/uploaders/ckeditor_picture_uploader.rb
@@ -1,46 +1,30 @@
+# app/uploaders/ckeditor_picture_uploader.rb
 # frozen_string_literal: true
 
 class CkeditorPictureUploader < CarrierWave::Uploader::Base
   include Ckeditor::Backend::CarrierWave
-
-  # Include RMagick or ImageScience support:
-  # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
-  # include CarrierWave::ImageScience
 
-  # Choose what kind of storage to use for this uploader:
   storage :file
 
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/ckeditor/pictures/#{model.id}"
   end
 
-  # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  def filename
+    # Sanitiza el nombre del archivo para eliminar caracteres especiales
+    "#{super.gsub(/[^0-9A-Za-z.\-]/, '_')}" if original_filename
+  end
 
-  # Process files as they are uploaded:
-  # process scale: [200, 300]
-  #
-  # def scale(width, height)
-  #   # do something
-  # end
-
-  # Create different versions of your uploaded files:
   version :thumb do
-    process resize_to_fill: [118, 100]
+    process resize_to_fill: [ 118, 100 ]
   end
 
   version :content do
-    process resize_to_limit: [800, 800]
+    process resize_to_limit: [ 800, 800 ]
   end
 
-  # Add a white list of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
-  def extension_white_list
+  def extension_allowlist
     Ckeditor.image_file_types
   end
 end
