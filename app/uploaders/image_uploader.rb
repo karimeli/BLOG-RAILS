84b9882bd12d79bb64688d3024e83b967547df9b
karimeli <karimcarlos32@icloud.com>
2025-10-27 11:35:05 -0600

    post

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,9 +1,21 @@
 # app/uploaders/image_uploader.rb
 class ImageUploader < CarrierWave::Uploader::Base
+  # Incluye MiniMagick para procesamiento
+  include CarrierWave::MiniMagick
+
   storage :file
 
-  # Define el tipo de archivo que quieres permitir.
-  # Devolver un array vacío o nil permite todos los formatos.
+  # Define dónde se guardarán los archivos
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  # Crea una versión más pequeña de la imagen
+  version :thumb do
+    process resize_to_fill: [ 100, 100 ]
+  end
+
+  # Permite cualquier formato de imagen
   def extension_allowlist
     nil
   end
